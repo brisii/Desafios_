c020252a530935e647e15df7eb539840158f018d
BJimenez <jjimenezu@miumg.edu.gt>
2023-04-01 21:55:15 -0700

    desafios

---
+++ calculadora/base datos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+class Calculadora
+{
+    public string Marca { get; set; }
+    public string Serie { get; set; }
+
+    public int Sumar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Restar(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public int Multiplicar(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Dividir(int num1, int num2)
+    {
+        if (num2 == 0)
+        {
+            throw new DivideByceroException("No se puede dividir por cero.");
+        }
+        return (double)num1 / num2;
+    }
+}
+
+[Serializable]
+internal class DivideByceroException : Exception
+{
+    public DivideByceroException()
+    {
+    }
+
+    public DivideByceroException(string message) : base(message)
+    {
+    }
+
+    public DivideByceroException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected DivideByceroException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
---
+++ e-mail/Desafio2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+interface INotificable
+{
+    void Notifica(string mensaje);
+}
+
+class NotificacionEmail : INotificable
+{
+    public string direccionCorreo;
+
+    public void Notifica(string mensaje)
+    {
+        Console.WriteLine($"Enviando email a {direccionCorreo}: {mensaje}");
+        // Aquí iría el código para enviar un email a la dirección especificada
+    }
+}
+
+class NotificacionWhatsap : INotificable
+{
+    public string numeroTelefono;
+
+    public void Notifica(string mensaje)
+    {
+        Console.WriteLine($"Enviando mensaje por WhatsApp a {numeroTelefono}: {mensaje}");
+        // Aquí iría el código para enviar un mensaje por WhatsApp al número especificado
+    }
+}
+
+class NotificacionSMS : INotificable
+{
+    public string numeroTelefono;
+
+    public void Notifica(string mensaje)
+    {
+        Console.WriteLine($"Enviando SMS a {numeroTelefono}: {mensaje}");
+        // Aquí iría el código para enviar un SMS al número especificado
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var notificacionEmail = new NotificacionEmail { direccionCorreo = "miumg@.edu.gt.ejemplo.com" };
+        var notificacionWhatsap = new NotificacionWhatsap { numeroTelefono ="45104030" };
+        var notificacionSMS = new NotificacionSMS { numeroTelefono = "55237880" };
+
+        notificacionEmail.Notifica("Hola desde MJexpresrapid ");
+        notificacionWhatsap.Notifica("Hola desde Mjexpresrapid ");
+        notificacionSMS.Notifica("Hola desde Mjexpresrapid ");
+    }
+}
